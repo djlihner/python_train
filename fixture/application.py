9b02ca4bd26666640e158c6ceb8662e9e8913f9d
Dmitriy Lihner <likstern@gmail.com>
2018-01-22 05:01:54 +0300

    Добавлено предусловие для удаления группы

--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,7 @@ class Application:
 
     def __init__(self):
         self.wd = webdriver.Chrome(executable_path='D:\\Coding\\PyLerning\\venv\\chromedriver\\chromedriver.exe')
-        self.wd.implicitly_wait(60)
+        self.wd.implicitly_wait(5)
         # при конструированни драйвера передаем ссылку на фикстуру
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
--- tests/test_modify_group.py
+++ tests/test_modify_group.py
@@ -4,8 +4,9 @@ from model.group import Group
 def test_modify_group_name(app):
     app.group.modify_first(Group(name="New group"))
 
-# def test_modify_group_header(app):
-#     app.session.login(user="admin", password="secret")
-#     app.group.modify_first(Group(header="New header"))
-#     app.session.logout()
-#
+
+def test_modify_group_header(app):
+    app.session.login(user="admin", password="secret")
+    app.group.modify_first(Group(header="New header"))
+    app.session.logout()
+
